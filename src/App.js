feafc372746caf84534c70016a334eb3b3d5862d
unknown <tarekyosry8@gmail.com>
2023-07-06 19:33:47 +0300

    create navigation and hero

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import { Hero } from './components/Hero';
+import { Navigation } from './components/Navigation';
+import { Card } from './components/Card';
 
 function App() {
   return (
     <div className="App">
-      
+      <Navigation /> 
+      <Hero />
+      {/* <Card /> */}
     </div>
   );
 }
