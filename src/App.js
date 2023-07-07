f5c83bd4587b4c3b570074b827dae9f13c0250d8
unknown <tarekyosry8@gmail.com>
2023-07-07 23:13:22 +0300

    import all data dynamically

--- src/App.js
+++ src/App.js
@@ -2,21 +2,30 @@ import './App.css';
 import { Hero } from './components/Hero';
 import { Navigation } from './components/Navigation';
 import { Card } from './components/Card';
-import swimmer from "./images/swimmer.png"
+// import swimmer from "./images/katie-zaferes.png"
+
+import data from "./components/data"
 
 function App() {
+  const cards = data.map(card => {
+    return <Card 
+            id = {card.id}
+            title = {card.title}
+            description= {card.description}
+            price = {card.price}
+            coverImg= {card.coverImg}
+            rating={card.stats.rating}
+            reviewCount={card.stats.reviewCount}
+            location={card.location}
+            openSpots= {card.openSpots}
+          />
+  })
+
   return (
     <div className="App">
       <Navigation /> 
       <Hero />
-      <Card 
-        img= {swimmer}
-        rating= {5.0}
-        reviewCount= {6}
-        country= "Egypt"
-        title= "Life lessons with Katie Zaferes"
-        price= {136}
-      />
+      {cards}
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,19 +1,18 @@
 import React from 'react'; 
 import star from '../images/Star 1.png'
-// import swimmer from '../images/swimmer.png'
+import swimmer from '../images/katie-zaferes.png'
 
-export const Card = ({img, title, price, reviewCount, rating, country}) => {
+export const Card = ({coverImg, title, price, reviewCount, rating, location}) => {
   return (
     <div className='card'>
         <div className='card-img'>
-            <img alt='swimmer' src={img}></img>
-            {/* <button>Sold Out</button> */}
+            <img alt='swimmer' src={`/images/${coverImg}`}></img>
         </div>
         <div className='card-info'>
             <img src={star} className='star-img'></img>
             <h5 className='star-rate'>{rating}</h5>
             <h5 className='star-num'>({reviewCount})</h5>
-            <h5 className='star-country'>. {country}</h5>
+            <h5 className='star-country'>. {location}</h5>
         </div>
         <p>{title}</p>
         <h4>From ${price} <span className='person'>/ person</span></h4>
